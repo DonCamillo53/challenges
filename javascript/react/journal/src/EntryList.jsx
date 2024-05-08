085595660d9174d1d5869e942c346c6b42b18d1a
Camillo Hochberg <camillo.h@hotmail.de>
2024-05-08 10:00:18 +0200

    small changes

--- javascript/react/journal/src/EntryList.jsx
+++ javascript/react/journal/src/EntryList.jsx
@@ -3,8 +3,13 @@ export function Entry({ date, entryHeadline, entryText }) {
     <ul>
       <article>
         <h2>FEB 27, 2028</h2>
-        <h3>"That's life in the city"</h3>
-        <img src="./src/assets/star-filled.svg" alt="Add or remove Favorites" />
+        <div className="headline-container">
+          <h3>"That's life in the city"</h3>
+          <img
+            src="./src/assets/star-filled.svg"
+            alt="Add or remove Favorites"
+          />
+        </div>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
           laudantium, illum temporibus officiis fugit consectetur totam maiores
